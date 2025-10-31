0d948239192de87e3b0a1a4cc058f93690b7c5b5
Allan Guilherme <153043227+All4mn@users.noreply.github.com>
2025-10-31 11:32:43 -0300

    menu inicio

--- src/components/components/menuInicial/MenuInicial.jsx
+++ src/components/components/menuInicial/MenuInicial.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import styles from "./MenuInicial.module.css";
 
 export const MenuInicial = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <h1>Menu</h1>
+      <div>
+      <button>Meu Clã</button>
+      <button>Jogar</button>
+      <button>Rankings</button>
+      </div>
     </div>
   )
 }
