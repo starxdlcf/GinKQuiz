14f9e16974bf81e0cbe6b030a298a09d16edfe63
Allan Guilherme <153043227+All4mn@users.noreply.github.com>
2025-11-11 11:53:48 -0300

    login

--- src/Supabase.jsx
+++ src/Supabase.jsx
@@ -1,8 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
+const URL = import.meta.env.VITE_SUPABASE_URL;
+const KEY = import.meta.env.VITE_SUPABASE_KEY;
 
-const url = "https://txykbjumieeifxygbpow.supabase.co";
-
-const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR4eWtianVtaWVlaWZ4eWdicG93Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEwNDQ5OTUsImV4cCI6MjA3NjYyMDk5NX0.0o5erJUFnqfaIK6KiSmi9HK2IWLOKTsBVbF56rCzC-o"
-
-export const supabase = createClient(url, key);
+export const supabase = createClient(URL, KEY);
