c5a80aaee1737c1723750adfcdf4ff50a1a6357f
Maagarcia <153952539+Maagarcia@users.noreply.github.com>
2025-11-14 09:33:19 -0300

    jogo do erro completo

--- src/Supabase.jsx
+++ src/Supabase.jsx
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const URL = import.meta.env.VITE_SUPABASE_URL;
-const KEY = import.meta.env.VITE_SUPABASE_KEY;
+// const KEY = import.meta.env.supabase_key;
+const KEY= import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(URL, KEY);
--- src/components/components/error/Error.jsx
+++ src/components/components/error/Error.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { use } from "react";
 import styles from "./Erro.module.css";
 import { useEffect } from "react";
 import { useState } from "react";
@@ -7,29 +7,87 @@ import { useState } from "react";
 const arrayPalavras = [
   "quiz",
   "jogo",
-  "diversao",
+  "diversão",
   "matematica",
   "portugues",
   "quimica",
   "fisica",
+  "historia",
+  "geografia",
+  "biologia",
+  "computador",
+  "programação",
+  "desenvolvimento",
+  "internet",
+  "tecnologia",
+  "ciência",
+  "engenharia",
+  "arte",
+  "música",
+  "literatura",
+  "filosofia",
+  "sociologia",
+  "psicologia",
+  "economia",
+  "negócios",
+  "marketing",
+  "finanças",
+  "empreendedorismo",
+  "educação",
+  "saúde",
+  "esporte",
+  "viagem",
+  "mafagafos"
 ];
 let palavraAleatoria =
 arrayPalavras[Math.floor(Math.random() * arrayPalavras.length)];
 
 export const Error = () => {
-  const [palavrasplit,setPalavrasplit] = useState([]);
+  const TEMPO_INICIAL = 5;
+  
+  const [game,setGame] = React.useState(false);
+
+  const [seconds, setSeconds] = React.useState(TEMPO_INICIAL);
+
+  const [running, setRunning] = React.useState(false);
 
   const [palavra, setPalavra] = React.useState("");
 
   const [pontos, setPontos] = React.useState(0);
 
+  const palavraRef = React.useRef(null);
+
+  const segundoRef = React.useRef(null);
+
 useEffect(()=>{
-  document.addEventListener('keydown',handlekey)
+  if(palavraRef.current){
+    palavraRef.current.classList.remove(styles.bounce);
+    void palavraRef.current.offsetWidth; // força reinício
+    palavraRef.current.classList.add(styles.bounce);
+  }}, [palavraAleatoria])
+useEffect(()=>{
+  if(segundoRef.current){
+    segundoRef.current.classList.remove(styles.bounce);
+    void segundoRef.current.offsetWidth; // força reinício
+    segundoRef.current.classList.add(styles.bounce);
+  }}, [seconds])
+
+
+  useEffect(() => {
+    let interval = null;
+    if (running && seconds > 0) {
+      interval = setInterval(() => {
+        setSeconds((seconds) => seconds - 1);
+      }, 1000);
+    } else if (running && seconds === 0) {
+      alert(`Fim de jogo! Você fez ${pontos} pontos.`);
+      setRunning(false);
+      setGame(false);
+    }
+  return () => clearInterval(interval);
   
-  return () =>{
-    document.removeEventListener('keydown', handlekey)
-  }
-},[])
+  },[pontos, seconds, running]);
+
 
   const handleChange = (event) => {
     setPalavra(event.target.value);
@@ -38,6 +96,7 @@ useEffect(()=>{
     if (event.target.value.length === palavraAleatoria.length) {
       if (event.target.value == palavraAleatoria) {
         setPontos((num)=> num+1);
+        setSeconds((segundos) => segundos + 1);
         console.log('oioi')
       } else {
         console.log("erro");
@@ -48,15 +107,22 @@ useEffect(()=>{
     }
   };
 
-  const handlekey = (event) => {
-    setPalavrasplit(palavrasplit => [...palavrasplit,event.key])
-   setPalavra(event.key) 
-    console.log(palavrasplit)
+  // const handlekey = (event) => {
+  //   setPalavrasplit(palavrasplit => [...palavrasplit,event.key])
+  //  setPalavra(event.key) 
+  //   console.log(palavrasplit)
+  // }
+
+  const startGame = () => {
+    setGame(true);
+    setRunning(true);
+    setSeconds(TEMPO_INICIAL);
+    setPontos(0);
   }
 
 
   return (
-    <div onKeyDown={handlekey}>
+    <div>
       <div className={styles.paginaErro} >
         <h1>Erro 404</h1>
 
@@ -68,16 +134,27 @@ useEffect(()=>{
               divertido?
             </h2>
             <h2>Ou se quiser jogar outra coisa</h2>
+
+            {game ===true?
+            <>
             <input
-              className={styles.input}
-              value={palavra}
-              onChange={handleChange}
-              type="text"
-              name=""
-              id=""
-            />
-            <h3>{palavraAleatoria}</h3>
-            <h3>{pontos}</h3>
+            className={styles.input}
+                value={palavra}
+                onChange={handleChange}
+                type="text"
+                name=""
+                id=""
+              />
+  
+              <h3 ref={palavraRef}>{palavraAleatoria}</h3>
+              <h3 ref={segundoRef}>{pontos}</h3>
+              <h3>Tempo restante:{seconds}</h3>
+            
+            </>
+          : 
+          <button onClick={startGame} >desafio das palavras</button>
+          
+          }
           </div>
         </div>
       </div>
