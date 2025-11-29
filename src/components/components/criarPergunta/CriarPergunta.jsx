bd2a2abc0f469c043f0ea44b8120b8270538c038
Maagarcia <153952539+Maagarcia@users.noreply.github.com>
2025-11-29 00:04:45 -0300

    exibit perguntas

---
+++ src/components/components/criarPergunta/CriarPergunta.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const CriarPergunta = () => {
+  return (
+    
+    <div>CriarPergunta</div>
+  )
+}
+
+export default CriarPergunta
--- src/components/components/gerenciamento/Gerenciamento.jsx
+++ src/components/components/gerenciamento/Gerenciamento.jsx
@@ -1,33 +1,69 @@
-import React from 'react'
-import { supabase } from '../../../Supabase'
-
+import React from "react";
+import { supabase } from "../../../Supabase";
 
 const Gerenciamento = () => {
   const id = localStorage.getItem("userId");
-  
+
   const [temas, setTemas] = React.useState(null);
   const [perguntas, setPerguntas] = React.useState(null);
-
 
   React.useEffect(() => {
     fetchTemas();
     console.log("ID do usuario no gerenciamento:", id);
-    console.log("Temas:", temas);
   }, []);
 
   const fetchTemas = async () => {
-    const{data} = await supabase
-    .from('temas')
-    .select('*')
+    const { data } = await supabase.from("temas").select("*");
     setTemas(data);
-  }
+  };
+
+  const fetchPerguntas = async (id_tema) => {
+    const { data } = await supabase
+      .from("perguntas")
+      .select("*")
+      .eq("tema_pergunta", id_tema);
+    setPerguntas(data);
+    console.log(data);
+  };
+
   return (
     <>
-    <div>Gerenciamento</div>
+      <div>Gerenciamento</div>
 
-    <table></table>
+      <div style={{ display: "flex" }}>
+        <table>
+          <tbody>
+            {temas &&
+              temas.map((tema) => (
+                <tr key={tema.id_tema}>
+                  <td onClick={() => fetchPerguntas(tema.id_tema)}>
+                    {tema.nome_tema}
+                  </td>
+                </tr>
+              ))}
+            <tr></tr>
+          </tbody>
+        </table>
+        <table>
+          <tbody>
+            {perguntas &&
+              perguntas.map((pergunta) => (
+                <tr key={pergunta.id_pergunta}>
+                  <td>{pergunta.enunciado_pergunta}</td>
+                  <td>atualizar</td>
+                  <td>deletar</td>
+                </tr>
+              ))}
+            <tr>
+              <td>criar uma nova</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <button onClick={() => console.log(temas)}>mostrar temas</button>
     </>
-  )
-}
+  );
+};
 
-export default Gerenciamento
+export default Gerenciamento;
